202ea4ab2c13945c4c58d3ee0e08f874904b003b
Gilad Opher <gilad.opher@moovitapp.com>
2017-05-06 22:10:42 +0300

    replace api key with new one

--- app/src/main/java/com/imagesearch/searchresults/model/api/FlickerApi.java
+++ app/src/main/java/com/imagesearch/searchresults/model/api/FlickerApi.java
@@ -16,8 +16,7 @@ import retrofit2.http.Query;
  */
 public interface FlickerApi{
 
-
-	@GET("?method=flickr.photos.search&api_key=3e7cc266ae2b0e0d78e279ce8e361736&format=json&nojsoncallback=1&safe_search=1")
+	@GET("?method=flickr.photos.search&api_key=a933db0988735825c98458892db8a853&format=json&nojsoncallback=1&safe_search=1")
 	Observable<ImagesResultObject> getImages(@Query("text") String query, @Query("page") int page);
 
 
